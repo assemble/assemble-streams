e7e2fb1d1d335156a3b7ea6281a20399f64e9f54
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-10 11:57:54 -0400

    lint

--- index.js
+++ index.js
@@ -9,40 +9,38 @@
 
 var utils = require('./utils');
 
-module.exports = function (options) {
-  return function(app) {
+module.exports = function (app) {
 
-    /**
-     * Push a view collection into a vinyl stream.
-     *
-     * ```js
-     * app.toStream('posts', function(file) {
-     *   return file.path !== 'index.hbs';
-     * })
-     * ```
-     * @name .toStream
-     * @param {String} `collection` Name of the collection to push into the stream.
-     * @param {Function} Optionally pass a filter function to use for filtering views.
-     * @return {Stream}
-     * @api public
-     */
+  /**
+   * Push a view collection into a vinyl stream.
+   *
+   * ```js
+   * app.toStream('posts', function(file) {
+   *   return file.path !== 'index.hbs';
+   * })
+   * ```
+   * @name .toStream
+   * @param {String} `collection` Name of the collection to push into the stream.
+   * @param {Function} Optionally pass a filter function to use for filtering views.
+   * @return {Stream}
+   * @api public
+   */
 
-    app.toStream = function(name) {
-      var stream = utils.through.obj();
-      var src = utils.srcStream;
-      stream.setMaxListeners(0);
+  app.toStream = function (name) {
+    var stream = utils.through.obj();
+    var src = utils.srcStream;
+    stream.setMaxListeners(0);
 
-      if (typeof name === 'undefined') {
-        process.nextTick(stream.end.bind(stream));
-        return src(stream);
-      }
-
-      var views = this.getViews(name);
-      setImmediate(function () {
-        for (var key in views) stream.write(views[key]);
-        stream.end();
-      });
+    if (typeof name === 'undefined') {
+      process.nextTick(stream.end.bind(stream));
       return src(stream);
-    };
+    }
+
+    var views = this.getViews(name);
+    setImmediate(function () {
+      for (var key in views) stream.write(views[key]);
+      stream.end();
+    });
+    return src(stream);
   };
 };
