d356b8528e57f65c56f79d49d73932d5c3eb74c2
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-10 11:46:48 -0400

    lint

--- utils.js
+++ utils.js
@@ -4,12 +4,16 @@
  * Mdule dependencies
  */
 
-var lazy = require('lazy-cache')(require);
+var utils = require('lazy-cache')(require);
 var fn = require;
 
-require = lazy;
+require = utils;
 require('through2', 'through');
 require('src-stream');
 require = fn;
 
-module.exports = require;
+/**
+ * Expose `utils
+ */
+
+module.exports = utils;
